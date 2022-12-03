4256aaa28057a16d9c543bbeac0b127dad4fe906
KAC CHAN <kacxxchan@gmail.com>
2022-12-03 22:13:27 +0530

    Update reqgban.py

--- TOGA/modules/reqgban.py
+++ TOGA/modules/reqgban.py
@@ -8,13 +8,13 @@ from pyrogram.types import (
     Message,
 )
 
-from SUMI import pbot as Client
-from SUMI import (
+from TOGA import pbot as Client
+from TOGA import (
     OWNER_ID as owner_id,
     OWNER_USERNAME as owner_usn,
     SUPPORT_CHAT as log,
 )
-from SUMI.utils.errors import capture_err
+from TOGA.utils.errors import capture_err
 
 
 def content(msg: Message) -> [None, str]:
