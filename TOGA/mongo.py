d86e754abc31ab6369e60f8cded524df2cc5f22f
KAC CHAN <kacxxchan@gmail.com>
2022-12-03 15:34:38 +0530

    Update mongo.py

--- TOGA/mongo.py
+++ TOGA/mongo.py
@@ -3,8 +3,8 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from SUMI import MONGO_DB_URI
-from SUMI.conf import get_int_key, get_str_key
+from TOGA import MONGO_DB_URI
+from TOGA.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
