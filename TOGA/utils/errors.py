1c543b5f2615d9093bc29ae985c488744243e811
KAC CHAN <kacxxchan@gmail.com>
2022-12-03 16:00:43 +0530

    Update errors.py

--- TOGA/utils/errors.py
+++ TOGA/utils/errors.py
@@ -1,8 +1,7 @@
-
 import sys
 import traceback
 from functools import wraps
-from SUMI import pgram, SUPPORT_CHAT
+from TOGA import pgram, SUPPORT_CHAT
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
 
