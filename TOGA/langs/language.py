7c65c7cf69c467ccbd59e51b8203d02f989ab445
KAC CHAN <kacxxchan@gmail.com>
2022-12-03 15:42:07 +0530

    Update language.py

--- TOGA/langs/language.py
+++ TOGA/langs/language.py
@@ -19,11 +19,11 @@ class Language:
             return en_string
 
     def reload_strings(self) -> None:
-        for filename in os.listdir(r"./SUMI/langs"):
+        for filename in os.listdir(r"./TOGA/langs"):
             if filename.endswith(".yaml"):
                 language_name = filename[:-5]
                 self.languages[language_name] = yaml.safe_load(
-                    open(r"./SUMI/langs/" + filename, encoding="utf8")
+                    open(r"./TOGA/langs/" + filename, encoding="utf8")
                 )
 
     def get_languages(self) -> Dict:
