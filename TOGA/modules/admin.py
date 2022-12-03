0915ab27d1da0881085d6fa74a61d9e50017d5e3
KAC CHAN <kacxxchan@gmail.com>
2022-12-03 21:18:48 +0530

    Update admin.py

--- TOGA/modules/admin.py
+++ TOGA/modules/admin.py
@@ -12,9 +12,9 @@ from telethon.errors import *
 from telethon.tl import *
 from telethon import *
 
-from SUMI import DRAGONS, DEV_USERS, OWNER_ID, dispatcher, telethn as bot
-from SUMI.modules.disable import DisableAbleCommandHandler
-from SUMI.modules.helper_funcs.chat_status import (
+from TOGA import DRAGONS, DEV_USERS, OWNER_ID, dispatcher, telethn as bot
+from TOGA.modules.disable import DisableAbleCommandHandler
+from TOGA.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -27,12 +27,12 @@ from SUMI.modules.helper_funcs.chat_status import (
     can_manage_voice_chats,
 )
 
-from SUMI.modules.helper_funcs.extraction import (
+from TOGA.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from SUMI.modules.log_channel import loggable
-from SUMI.modules.helper_funcs.alternate import send_message
+from TOGA.modules.log_channel import loggable
+from TOGA.modules.helper_funcs.alternate import send_message
 
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
