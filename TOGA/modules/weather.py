7598e730273b502d133c1b9713f324f53cf617a7
KAC CHAN <kacxxchan@gmail.com>
2022-12-03 22:25:49 +0530

    Update weather.py

--- TOGA/modules/weather.py
+++ TOGA/modules/weather.py
@@ -1,4 +1,4 @@
-from SUMI import telethn as tbot
+from TOGA import telethn as tbot
 import io
 import os
 import time
@@ -9,9 +9,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from SUMI import *
+from TOGA import *
 
-from SUMI.events import register
+from TOGA.events import register
 
 
 async def is_register_admin(chat, user):
