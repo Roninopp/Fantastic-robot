7900cea5d9e69889eb5630f5c441f5d816bc5448
KAC CHAN <kacxxchan@gmail.com>
2022-12-04 09:45:42 +0530

    Update locks_sql.py

--- TOGA/modules/sql/locks_sql.py
+++ TOGA/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from SUMI.modules.sql import SESSION, BASE
+from TOGA.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
