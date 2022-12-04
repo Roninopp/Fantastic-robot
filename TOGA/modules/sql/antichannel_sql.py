a9b8d807a403e3bf4b2e88922c8f9c52cc7b9b95
KAC CHAN <kacxxchan@gmail.com>
2022-12-04 09:41:53 +0530

    Update antichannel_sql.py

--- TOGA/modules/sql/antichannel_sql.py
+++ TOGA/modules/sql/antichannel_sql.py
@@ -4,7 +4,7 @@ from sqlalchemy import Boolean
 from sqlalchemy.sql.sqltypes import String
 from sqlalchemy import Column
 
-from SUMI.modules.sql import BASE, SESSION
+from TOGA.modules.sql import BASE, SESSION
 
 
 class AntiChannelSettings(BASE):
