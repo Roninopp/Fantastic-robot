34fe0703f015fce7d21a6641fb09b4dfd52bfc7e
KAC CHAN <kacxxchan@gmail.com>
2022-12-04 09:46:14 +0530

    Update logger_sql.py

--- TOGA/modules/sql/logger_sql.py
+++ TOGA/modules/sql/logger_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from SUMI.modules.sql import BASE, SESSION
+from TOGA.modules.sql import BASE, SESSION
 
 class LoggerSettings(BASE):
     __tablename__ = "chat_log_settings"
