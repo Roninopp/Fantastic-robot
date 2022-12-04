bd5a50432c271a1d136b2753a04702eea9640089
KAC CHAN <kacxxchan@gmail.com>
2022-12-04 09:49:37 +0530

    Update welcome_sql.py

--- TOGA/modules/sql/welcome_sql.py
+++ TOGA/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from SUMI.modules.helper_funcs.msg_types import Types
-from SUMI.modules.sql import BASE, SESSION
+from TOGA.modules.helper_funcs.msg_types import Types
+from TOGA.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you? 🐿️"
