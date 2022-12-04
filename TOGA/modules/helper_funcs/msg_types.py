6771c2c80f8cb24deb6cacdb031c26b27c5cf9d7
KAC CHAN <kacxxchan@gmail.com>
2022-12-04 09:56:52 +0530

    Update msg_types.py

--- TOGA/modules/helper_funcs/msg_types.py
+++ TOGA/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from SUMI.modules.helper_funcs.string_handling import \
+from TOGA.modules.helper_funcs.string_handling import \
     button_markdown_parser
 from telegram import Message
 
