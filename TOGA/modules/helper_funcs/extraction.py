2a743da070d08b47aa7bbc73098387c0053a3c02
KAC CHAN <kacxxchan@gmail.com>
2022-12-04 09:54:43 +0530

    Update extraction.py

--- TOGA/modules/helper_funcs/extraction.py
+++ TOGA/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from SUMI import LOGGER
-from SUMI.modules.users import get_user_id
+from TOGA import LOGGER
+from TOGA.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
