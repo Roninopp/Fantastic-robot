e3e34f54acc1363eda29fbfbe54cf2c98d1aaa48
KAC CHAN <kacxxchan@gmail.com>
2022-12-09 18:50:05 +0530

    Update __init__.py

--- TOGA/__init__.py
+++ TOGA/__init__.py
@@ -200,6 +200,7 @@ else:
     ARQ_API_URL = Config.ARQ_API_URL
     ARQ_API_KEY = Config.ARQ_API_KEY
     COTB = Config.COTB
+    INFOPIC = Config.INFOPIC
 
     try:
         BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
