411a24039a41fb5dd49817b04efcc1ca65858c07
KAC CHAN <kacxxchan@gmail.com>
2022-12-11 17:41:10 +0530

    Update config.py

--- TOGA/config.py
+++ TOGA/config.py
@@ -51,7 +51,7 @@ class Config(object):
     STRICT_GBAN=True
     REDIS_URL="redis://betatoga:Betatoga123+@redis-15793.c241.us-east-1-4.ec2.cloud.redislabs.com:15793"
     UPDATE_CHANNEL="TogaUpdates"
-    MONGO_DB_URI="mongodb+srv://suguha:suguha123@cluster0.mzvqn35.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_DB_URI="mongodb+srv://kacbusiness:hrituzee123@kacbusiness.dahr62x.mongodb.net/?retryWrites=true&w=majority"
     BOT_USERNAME="Toga_Robot"
     REM_BG_API_KEY=""
     CASH_API_KEY=""
