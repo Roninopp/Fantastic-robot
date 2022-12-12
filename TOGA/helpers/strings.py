b6d3dec01a5b2a3adbaade32e72b2c5ad115e4e1
KAC CHAN <kacxxchan@gmail.com>
2022-12-12 06:34:42 +0530

    Create strings.py

---
+++ TOGA/helpers/strings.py
@@ -0,0 +1,6 @@
+from TOGA import __version__
+from platform import python_version
+from telegram import __version__ as _libv_
+
+SPT_LOGIN = "Hey {}, Please click the button below to login with your spotify account."
+SPT_LOGIN_PM = "Please contact me in PM to login with your spotify account, inorder to use this feature."
