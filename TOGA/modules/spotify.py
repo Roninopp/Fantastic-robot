2baa0bc8b9a7ccb78df6c8cd1bf255cbc05780d3
KAC CHAN <kacxxchan@gmail.com>
2022-12-12 07:31:37 +0530

    Update spotify.py

--- TOGA/modules/spotify.py
+++ TOGA/modules/spotify.py
@@ -2,7 +2,7 @@ from telegram.ext import CommandHandler
 from telegram.ext.dispatcher import run_async
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
-import dp, typing
+import typing
 from TOGA.helpers.spthelper import SpotifyClient, get_spotify_data
 import TOGA.helpers.strings as st
 
